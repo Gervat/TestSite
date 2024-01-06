52b77182111f55015be07586af0eeb8a7afeba02
Gervat <mannyda@hotmail.com>
2024-01-06 11:48:18 -0600

    .

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1704560336|306506874";
+const CACHE_VERSION = "1704563283|3254317376";
 const CACHE_PREFIX = "Test Website-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
